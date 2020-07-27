7e3f1946070debf526ff430eeb87d67a5c3b3ba5
Cody Krivacic <codykrivacic@Codys-MacBook-Pro.local>
2020-07-27 15:41:28 -0700

    use workspace-defined python path

--- roseasy/big_jobs.py
+++ roseasy/big_jobs.py
@@ -35,7 +35,7 @@ def submit(script, workspace, **params):
     qsub_command += '-t', '1-{0}'.format(nstruct),
     qsub_command += '-l', 'h_rt={0}'.format(max_runtime),
     qsub_command += '-l', 'mem_free={0}'.format(max_memory),
-    qsub_command += 'python3',
+    qsub_command += workspace.python_path,
     qsub_command += script,
     qsub_command += workspace.focus_dir,
 
