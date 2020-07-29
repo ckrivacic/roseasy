66b330bce1c59bba24eb291230fa278629249d41
Cody Krivacic <codykrivacic@Codys-MacBook-Pro.local>
2020-07-29 14:06:41 -0700

    roseasy now command-line accessible

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+def define_command(module, extras=None):
+    entry_point = '{0} = roseasy.commands.{0}:main'.format(module)
+    if extras is not None:
+        entry_point += ' {0}'.format(extras)
+    return entry_point
+
 setuptools.setup(
     name="roseasy-codykrivacic", # Replace with your own username
     version="0.0.1",
@@ -20,4 +26,15 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    entry_points={
+        'console_scripts':[
+            'roseasy=roseasy.main:main',
+            ],
+        'roseasy.commands': [
+            define_command('fetch_data'),
+            define_command('push_data'),
+            define_command('submit'),
+            define_command('plot_funnels')
+            ],
+        }
 )
