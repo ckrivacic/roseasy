ebc7d721f126fcf3c28eef2f410ecd0c0774e958
Cody Krivacic <codykrivacic@Codys-MBP.ucsfmedicalcenter.org>
2020-08-02 14:37:50 -0700

    functions for adding residues

--- roseasy/movers/mover.py
+++ roseasy/movers/mover.py
@@ -160,3 +160,96 @@ class Mover(object):
     def apply(self):
         '''Meant to be overriden in child classes.'''
         raise NotImplementedError
+
+
+def insert_alas(pose, position, length, insert_after=True, reset_fold_tree=True, fold_tree_root=1):
+    '''Insert a poly-ALA peptide before or after a given position.,
+    Set the fold tree to have a cutpoint before or after inserted residues.
+    Author: XingJie Pan
+    '''
+    assert(1 <= position <= pose.size())
+
+    # Set the fold tree with a single cutpoint
+
+    def sub_fold_tree_add_edges_no_jump(ft, root, start, stop):
+        '''Add edges to a sub-fold-tree that does not have
+        and jumps.'''
+        if start < root:
+            ft.add_edge(root, start, -1)
+        if stop > root:
+            ft.add_edge(root, stop, -1)
+
+    if reset_fold_tree:
+        cutpoint = position if insert_after else position - 1
+        ft = rosetta.core.kinematics.FoldTree()
+        
+        if fold_tree_root <= cutpoint and cutpoint < pose.size():
+            sub_root = pose.size()
+            ft.add_edge(fold_tree_root, sub_root, 1)
+            sub_fold_tree_add_edges_no_jump(ft, sub_root, cutpoint + 1, pose.size())
+            sub_fold_tree_add_edges_no_jump(ft, fold_tree_root, 1, cutpoint)
+        
+        elif fold_tree_root > cutpoint and cutpoint > 0:
+            sub_root = 1
+            ft.add_edge(fold_tree_root, sub_root, 1)
+            sub_fold_tree_add_edges_no_jump(ft, sub_root, 1, cutpoint)
+            sub_fold_tree_add_edges_no_jump(ft, fold_tree_root, cutpoint + 1, pose.size())
+
+        else:
+            sub_fold_tree_add_edges_no_jump(ft, fold_tree_root,  1, pose.size())
+        
+        pose.fold_tree(ft)
+
+    # Append the residues
+
+    residue_type_set = pose.residue_type_set_for_pose()
+    new_rsd = rosetta.core.conformation.ResidueFactory.create_residue( residue_type_set.name_map("ALA") )
+   
+    for i in range(length):
+        if insert_after:
+            pose.conformation().safely_append_polymer_residue_after_seqpos(new_rsd, position + i, True)
+            pose.set_omega(position + i, 180)
+        else:
+            pose.conformation().safely_prepend_polymer_residue_before_seqpos(new_rsd, position, True)
+            pose.set_omega(position, 180)
+
+    if insert_after:
+        rosetta.core.conformation.idealize_position(position + length, pose.conformation())
+        
+        if position + length + 1 <= pose.size():
+            rosetta.core.conformation.idealize_position(position + length + 1, pose.conformation())
+    else:
+        if position - 1 > 0:
+            rosetta.core.conformation.idealize_position(position - 1, pose.conformation())
+        rosetta.core.conformation.idealize_position(position, pose.conformation())
+
+
+def mutate_residues(pose, res_list, aa_list, protein_only=True):
+    '''Mutate a list of residues. The list of AAs could
+    either be 1 letter code or 3 letter code.
+    Author: XingJie Pan
+    '''
+    aa_name_map = {'A':'ALA', 'P':'PRO', 'V':'VAL', 'L':'LEU', 'I':'ILE', 'M':'MET',
+                   'F':'PHE', 'Y':'TYR', 'W':'TRP', 'S':'SER', 'T':'THR', 'C':'CYS',
+                   'K':'LYS', 'R':'ARG', 'H':'HIS', 'D':'ASP', 'E':'GLU', 'N':'ASN',
+                   'Q':'GLN', 'G':'GLY'}
+
+
+    mutater = rosetta.protocols.simple_moves.MutateResidue()
+    for i in range(len(res_list)):
+        if protein_only and (not pose.residue(res_list[i]).is_protein()):
+            continue
+
+        name = aa_list[i] if len(aa_list[i]) == 3 else aa_name_map[aa_list[i]]
+        mutater.set_res_name(name)
+        mutater.set_target(res_list[i])
+        mutater.apply(pose)
+
+
+def add_aas(pose, position, sequence, pdbnum=False, chain='A'):
+    if pdbnum:
+        position = pose.pdb_info().pdb2pose(position, chain)
+
+    insert_alas(pose, position, len(sequence))
+    mutate_residues(pose, list(range(position + 1, position + 1 +
+        len(sequence))), list(sequence), True)
