81f3f5178174e6d5798dd383d8f6f6601a9c548f
Cody Krivacic <codykrivacic@Codys-MacBook-Pro.local>
2020-08-19 12:18:24 -0700

    get rmsd of loop after run

--- roseasy/standard_params/rmsd.py
+++ roseasy/standard_params/rmsd.py
@@ -9,12 +9,26 @@ To do: Add option for recursive lookup.
 '''
 
 import docopt
+from pyrosetta.rosetta.core.pose import setPoseExtraScore
 from pyrosetta.rosetta.core.scoring import CA_rmsd
 from pyrosetta.rosetta.core.scoring import all_atom_rmsd
 from pyrosetta import pose_from_file
 from pyrosetta import init
-from roseasy import workspace
-import os
+from roseasy import pipeline
+import os, gzip
+
+def add_lines_to_gzip(fname, lines):
+    final_lines = []
+    with gzip.open(fname, 'rt') as f:
+        for line in f:
+            final_lines.append(line)
+
+    for line in lines:
+        final_lines.append(line + '\n')
+
+    with gzip.open(fname, 'wt') as f:
+        for line in final_lines:
+            f.write(line)
 
 def main():
     args = docopt.docopt(__doc__)
@@ -23,18 +37,19 @@ def main():
     target = pose_from_file(args['<input>'])
     workspace = pipeline.workspace_from_dir(folder)
     loop = workspace.largest_loop
-    residues = []
-    for i in range(int(loop.start), int(loop.end) + 1):
-        residues.append(i)
 
-    for root, dirs, files in os.walk(folder):
+    for root, dirs, files in os.walk(workspace.output_dir):
         for name in files:
             if name.endswith('.pdb.gz'):
-                mobile = pose_from_file(os.path.join(root, name))
-                rmsd = CA_rmsd(mobile, target)
-                print(rmsd)
-                rmsd = all_atom_rmsd(mobile, target)
-                print(rmsd)
+                pdbpath = os.path.join(root, name)
+                mobile = pose_from_file(pdbpath)
+                rmsd = CA_rmsd(mobile, target, int(loop.start), int(loop.end))
+
+                metric_name = 'EXTRA_METRIC_Loop_CA_RMSD [[-]]'
+
+                add_lines_to_gzip(pdbpath, [metric_name + ' ' + str(rmsd)])
+                #rmsd = all_atom_rmsd(mobile, target)
+                #print(rmsd)
 
 
 if __name__=='__main__':
