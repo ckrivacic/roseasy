20d46c6ba0ee419e7fd9a9852af19f619fd0ff46
Cody Krivacic <codykrivacic@Codys-MBP.ucsfmedicalcenter.org>
2021-03-21 20:46:20 -0700

    function for making relative resfile from regular resfile

--- roseasy/utils/lucs.py
+++ roseasy/utils/lucs.py
@@ -1,11 +1,11 @@
 import os
 import json
+from klab.rosetta import input_files
 
-def get_insertion(pdbpath):
+def get_insertion(pdbpath, workspace):
     # hardcoding for now
     insertion_path = os.path.join(
-            os.environ['HOME'],
-            'cas', 'dels', 'rec2_helix',
+            workspace.root_dir, '..',
             'lucs', 'data', 'compatible'
             )
     model_number = os.path.basename(pdbpath).split('.')[0].split('_')[-1]
@@ -18,3 +18,36 @@ def get_insertion(pdbpath):
     # Return only first insertion for this script because there is only
     # one
     return insertion[0]
+
+def relative_resfile(workspace, insertion):
+    '''
+    Convert a resfile to relative resfiles given a LUCS insertion.
+    Note: Resfile/original PDB must have Rosetta numbering, i.e. start
+    with the first residue having the number 1. Otherwise, the function
+    will require the original pose and the LUCS pose.
+    '''
+    resfile_parser = input_files.Resfile(input_resfile=workspace.resfile_path)
+    start = insertion['start']
+
+    chain = 'A'
+    designable = []
+    for res in resfile_parser.design[chain]:
+        if int(res) < insertion['start']:
+            designable.append(int(res))
+        else:
+            designable.append(int(res) + (insertion['stop'] -
+                insertion['start']))
+
+    designable.extend([j for j in range(insertion['start'],
+        insertion['stop'])])
+    repackable = []
+    if chain in resfile_parser.repack:
+        for res in resfile_parser.repack[chain]:
+            if int(res) < insertion['start']:
+                repackable.append(int(res))
+            else:
+                repackable.append(int(res) + (insertion['stop'] -
+                    insertion['start']))
+
+
+    return designable, repackable
